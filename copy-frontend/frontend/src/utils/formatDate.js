cec05dbe527510411deb216c50d22aa44ab12b79
mnaofficialbd <mna.official92@gmail.com>
2025-08-24 10:44:12 +0600

    assets update

--- copy-frontend/frontend/src/utils/formatDate.js
+++
@@ -1,7 +0,0 @@
-const formatDate = (dateString) => {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    const date = new Date(dateString);
-    return date.toLocaleDateString('en-US', options);
-};
-
-export default formatDate;
