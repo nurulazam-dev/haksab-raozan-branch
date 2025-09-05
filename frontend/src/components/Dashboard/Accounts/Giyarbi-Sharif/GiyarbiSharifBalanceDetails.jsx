1f6b66e3793b20f39c63a2d66226ea1d9ae68947
mnaofficialbd <mna.official92@gmail.com>
2025-09-06 00:24:08 +0600

    update Giyarbi-Sharif-Balance-Details component

--- frontend/src/components/Dashboard/Accounts/Giyarbi-Sharif/GiyarbiSharifBalanceDetails.jsx
+++ frontend/src/components/Dashboard/Accounts/Giyarbi-Sharif/GiyarbiSharifBalanceDetails.jsx
@@ -1,5 +1,6 @@
 import { useParams, Link } from "react-router-dom";
 import { gsBalancedata } from "../../../../assets/data/gsBalanceData";
+import logo from "../../../../assets/images/logo/abu_maola_rowja.png";
 
 const GiyarbiSharifBalanceDetails = () => {
   const { id } = useParams(); // get id from URL
@@ -23,15 +24,59 @@ const GiyarbiSharifBalanceDetails = () => {
 
   return (
     <div className="px-6 text-slate-800">
-      <h2 className="text-3xl font-bold mb-3 text-center text-blue-700">
-        GS Balance Details
+      {/* top section */}
+      <section className="mb-2">
+        <p className="text-center mb-2">Bismillahir Rahmanir Rahim</p>
+        <div className="relative py-2">
+          <img src={logo} alt="" className="mx-auto h-32 object-contain" />
+          <div className="flex justify-between items-center absolute inset-0 px-2">
+            <div className="text-start">
+              <p className="italic text-sm">Naraye Takbir</p>
+              <p className="italic text-sm">Naraye Risalat</p>
+              <p className="italic text-sm">Naraye Haydari</p>
+              <p className="italic text-sm">Naraye Hasnayin</p>
+              <p className="italic text-sm">Naraye Gaosiya</p>
+              <p className="italic text-sm">Naraye Gaosiya</p>
+              <p className="italic text-sm">Abu Maola Sultanpuri RH.</p>
+            </div>
+            <div className="text-end">
+              <p className="italic text-sm">Allahu Akbar</p>
+              <p className="italic text-sm">Yea Rasulullah S.A.W.</p>
+              <p className="italic text-sm">Yea Ali Martuza A.S.</p>
+              <p className="italic text-sm">Yea Hasan Yea Hossain A.S.</p>
+              <p className="italic text-sm">Yea Gaosul Azam Dastagir RH.</p>
+              <p className="italic text-sm">Gaosul Azam Maisvandary RH.</p>
+              <p className="italic text-sm">Marhaba Marhaba</p>
+            </div>
+          </div>
+        </div>
+        <div className="text-center">
+          <h2 className="text-2xl font-bold mb-1 text-slate-700">
+            Hazrat Abul Khair Sultanpuri RH. Association Bangladesh, <br />
+            (HASKAB) - Raozan Branch
+          </h2>
+          <p className="text-sm">
+            East Raozan, Rashidar Para, Joynagor-4340, Raozan, Chattogram.
+          </p>
+          <p className="text-sm">
+            <strong>Email:</strong> info@haskab-raozan-branch.org
+          </p>
+          <p>
+            <strong>Head Office:</strong> Satgachia Darbar Sharif, Mujibnagar,
+            Haidgaon, Patia, Chattogram.
+          </p>
+        </div>
+        <div className="border border-slate-400 mt-2"></div>
+      </section>
+      <h2 className="text-2xl font-bold mb-1 text-center bg-green-600 text-white py-1">
+        Giyarbi Sharif Balance Details
       </h2>
 
-      <div className="border border-blue-600 px-3 py-2 rounded-lg shadow-lg mb-4">
-        <h2 className="text-xl font-bold text-center underline underline-offset-4 mb-3">
+      <div className="px-3 py-1 mb-2">
+        {/* <h2 className="text-xl font-bold text-center underline underline-offset-4 mb-3">
           Balance Summary
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
+        </h2> */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-1">
           <p>
             <span className="font-semibold text-black">
               Giyarbi Sharif Date:
